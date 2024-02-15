6de86613c698b62b19c8c4c920390cac71de2dd7
fersayago <sayago.fernando@gmail.com>
2024-02-15 04:35:12 -0300

    feat: :sparkles: card actions
    added card actions to copy board link, rename and delete the card. Used 2 types of modals (one to confirm delete, another to change the name)

---
+++ components/confirm-modal.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from "./ui/alert-dialog";
+
+interface ConfirModalProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+  disabled?: boolean;
+  header: string;
+  description?: string;
+}
+
+const ConfirmModal = ({
+  children,
+  onConfirm,
+  disabled,
+  header,
+  description
+}: ConfirModalProps) => {
+  const handleConfirm = () => {
+    onConfirm();
+  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{header}</AlertDialogTitle>
+        <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleConfirm} disabled={disabled}>
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default ConfirmModal;
