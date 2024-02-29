ec74baaca8ac1c00d2db7061aec331a772ecc3d3
fersayago <sayago.fernando@gmail.com>
2024-02-29 16:16:12 -0300

    feat: :sparkles: cursor presence

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import { Camera } from "@/types/canvas";
+
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -26,4 +28,14 @@ export function cn(...inputs: ClassValue[]) {
 
 export function connectionIdToColor(connectionId: number): string {
   return COLORS[connectionId % COLORS.length];
+}
+
+export function pointerEventToCanvasPosition(
+  e: React.PointerEvent,
+  camera: Camera
+) {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
 }
